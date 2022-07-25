587499f04407e6910b5716faef0ed2053acaeb27
曾嘉彬 <1573439264@qq.com>
2022-07-25 22:20:20 +0800

    fir

---
+++ 240.搜索二维矩阵-ii.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=java
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0, j = n-1;
+        if(matrix[i][j] == target) return true;
+        while(i <= m-1 && j >= 0){
+            if(matrix[i][j] < target){
+                i++;
+            }else if(matrix[i][j] > target){
+                j--;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
