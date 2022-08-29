df17c3b00b150e29cd1a0e0ff46c7ea732485bc8
曾嘉彬 <1573439264@qq.com>
2022-08-29 10:57:52 +0800

    8.29

---
+++ 1470.重新排列数组.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1470 lang=java
+ *
+ * [1470] 重新排列数组
+ */
+
+// @lc code=start
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+
+    }
+}
+// @lc code=end
+
