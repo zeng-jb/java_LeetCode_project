ebb4b3c1482534eb6d1b66bafcedf171c19c59d5
曾嘉彬 <1573439264@qq.com>
2022-09-29 10:46:48 +0800

    9.29

---
+++ 670.最大交换.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+ * @lc app=leetcode.cn id=670 lang=java
+ *
+ * [670] 最大交换
+ */
+
+// @lc code=start
+class Solution {
+    public int maximumSwap(int num) {
+        char[] num1 = String.valueOf(num).toCharArray();
+        char[] num2 = String.valueOf(num).toCharArray();
+        Arrays.sort(num2);
+
+        Character lc = null, hc = null;
+        for (int i = 0; i < num1.length; i++) {
+            if (num1[i] != num2[num1.length - i - 1]) {
+                lc = num1[i];
+                hc = num2[num1.length - i - 1];
+                num1[i] = num2[num1.length - i - 1];
+                break;
+            }
+        }
+
+        if (lc != null) {
+            for (int i = num1.length - 1; i >= 0; i--) {
+                if (hc.equals(num1[i])) {
+                    num1[i] = lc;
+                    break;
+                }
+            }
+        }
+
+        return Integer.valueOf(new String(num1));
+    }
+}
+
+
+// @lc code=end
+
